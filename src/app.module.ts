42876d9153c13084a492cff195b627d0e4d188dd
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-11-20 16:30:16 -0300

    configuring typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          return {
+            type: 'better-sqlite3',
+            database: process.env.DB_DATABASE || './db.sqlite',
+            synchronize: process.env.DB_SYNCHRONIZE === '1',
+            autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+          };
+        }
+        throw new Error('Variáveis no .env estão incorretas!');
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,15 +5,20 @@ import {
   Param,
   ParseIntPipe,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly confiService: ConfigService) {}
+
   @Get(':id')
   findOne(
     @Param('id', CustomParseIntPipe)
     id: number,
   ) {
+    console.log(process.env.TESTE);
+    console.log(this.confiService.getOrThrow('TESTE', 'valorpadrao'));
     return `Olá do controle do user #${id}`;
   }
 }
