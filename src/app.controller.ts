9928ea699f61d1be5a0f4e3977f1f68bf15d0587
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-11-20 15:00:37 -0300

    cleaning project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('outra-rota')
+  getOutraRota(): string {
+    return this.appService.getOutraRota();
+  }
 }
